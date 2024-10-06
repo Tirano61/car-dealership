2cad04f473be7309d8caca8eb5dbba93bcdca1d6
Dario Ramirez <dariorama60@gmail.com>
2024-10-06 17:59:33 -0300

    CRUD brands

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { CarsModule } from './cars/cars.module';
+import { BrandsModule } from './brands/brands.module';
 
 
 
 @Module({
-  imports: [ CarsModule ],
+  imports: [ CarsModule, BrandsModule ],
   controllers: [],
   providers: [],
   exports: [ ]
